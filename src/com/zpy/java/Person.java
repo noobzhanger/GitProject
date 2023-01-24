77d03c25c8c8fd3f5f8870c710994f8e9127121c
zpy <592948255@qq.com>
2023-01-25 06:34:26 +0800

    dev first

--- src/com/zpy/java/Person.java
+++ src/com/zpy/java/Person.java
@@ -5,5 +5,6 @@ public class Person {
         System.out.println("hello world!");
         System.out.println("你好，世界");
         System.out.println("goodbye world");
+        System.out.println("还不睡");
     }
 }
